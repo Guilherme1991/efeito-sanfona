5b473866d4fd571bd7633c213b5d2369b0edba81
Guilherme1991 <gui23091991@gmail.com>
2019-08-07 00:50:50 -0300

    interacao sanfona

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let select = document.querySelector('.seletor')
+let selectUm = document.querySelector('.seletorUm')
+let selectDois = document.querySelector('.seletorDois')
+
+function ativa(){
+    select.classList.toggle('ativa')
+}
+
+function ativaUm(){
+    selectUm.classList.toggle('ativaUm')
+}
+
+function ativaDois(){
+    selectDois.classList.toggle('ativaDois')
+}
